14e984f756a0ace3f140f7397811ca51f030b78e
tttkieucntt2211013@student.ctuet.edu.vn <tttkieucntt2211013@student.ctuet.edu.vn>
2025-04-24 21:00:27 +0700

    lan1

---
+++ ShopBanBep/ShopBanBep/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopBanBep.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
